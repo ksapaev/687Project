2aae50f59ce2666f47c26f0b6f29429f2dba30fb
ksapaev <39306914+ksapaev@users.noreply.github.com>
2018-05-27 23:02:48 -0400

    saving LM_CheckIn.R

--- LM_CheckIn.R
+++ LM_CheckIn.R
@@ -14,8 +14,9 @@ df <- read.csv('data.csv')
 
 ####################################
 ##### write output file ############
-# add your R code to write LTR_CheckIn.png
+# add your R code to write LTR_HC.png
 ####################################
+
 
 
 
