5f0b07f0018759f54e6893c47e065e21bcc2fb7b
ksapaev <39306914+ksapaev@users.noreply.github.com>
2018-05-27 23:26:33 -0400

    saving LM_CheckOut.R

--- LM_CheckOut.R
+++ LM_CheckOut.R
@@ -14,8 +14,9 @@ df <- read.csv('data.csv')
 
 ####################################
 ##### write output file ############
-# add your R code to write LTR_StaffCared.png
+# add your R code to write LTR_RoomSatisfied.png
 ####################################
+
 
 
 
