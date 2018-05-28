8aa2d4058f9f7004c6cd95a0dcb5740cdebdeae3
ksapaev <39306914+ksapaev@users.noreply.github.com>
2018-05-27 22:50:09 -0400

    saving Associative_Rules.R

--- Associative_Rules.R
+++ Associative_Rules.R
@@ -10,11 +10,19 @@ df <- read.csv('data.csv')
 
 ## start writing your R code from here
 
+#Create Dataframe with appropriate columns
+NewDF <- data.frame(LengthOfStay=df$LengthStay, Gender=df$Gender, AgeRange=df$AgeRange, GPTier=df$GP_Tier, PurposeOfVisit = df$POV,
+                    State = df$StateAbb, Location = df$Location, Country = df$Country, NPS = df$NPS)
+
+
+#Convert column 'Length of stay' into a factor
+NewDF$LengthOfStay <- factor(NewDF[ ,1])
+
 ## end your R code and logic 
 
 ####################################
 ##### write output file ############
-write.csv(df, file = 'rules_data.csv')
+write.csv(NewDF, file = 'rules_data.csv')
 ####################################
 
 
