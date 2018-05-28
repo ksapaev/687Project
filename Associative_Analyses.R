c13896b67f62dc244a86d73854cea32514f89a9a
ksapaev <39306914+ksapaev@users.noreply.github.com>
2018-05-27 22:54:16 -0400

    saving Associative_Analyses.R

--- Associative_Analyses.R
+++ Associative_Analyses.R
@@ -97,8 +97,9 @@ dev.off()
 
 ####################################
 ##### write output file ############
-# add your R code to write DetConfidence.png
+# add your R code to write ProSupport.png
 ####################################
+
 
 
 
