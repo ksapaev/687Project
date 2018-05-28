5214d7fae8fa16f97ce464dc2d60fb6d25c9008f
ksapaev <39306914+ksapaev@users.noreply.github.com>
2018-05-28 19:22:48 -0400

    saving Descriptive_Statistics.R

--- Descriptive_Statistics.R
+++ Descriptive_Statistics.R
@@ -168,8 +168,9 @@ dev.off()
 
 ####################################
 ##### write output file ############
-# add your R code to write RoomSatisfaction.png
+# add your R code to write CheckIn.png
 ####################################
+
 
 
 
